9a8d1efd62996f9f65a7d31d533d1502c15444d5
fumetsuu <joseph.surin@gmail.com>
2019-03-10 20:49:10 +1100

    update scratchpads to use termite

--- .config/scripts/scratchpad-c.sh
+++ .config/scripts/scratchpad-c.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 code ~/Libraries/Code/scratchpads/C/scratchpad.cpp &&
-urxvt -e sh -c "echo 'C scratchpad file: scratchpad.cpp' && cd ~/Libraries/Code/scratchpads/C/ && bash"
+termite -d ~/Libraries/Code/scratchpads/C/
