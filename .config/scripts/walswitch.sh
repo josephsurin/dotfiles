82e74e9493bbeb8bf0caa8249bfa7de5cf89fe12
Joseph Surin <joseph.surin@gmail.com>
2020-10-31 19:35:02 +1100

    stop using wal

--- .config/scripts/walswitch.sh
+++ .config/scripts/walswitch.sh
@@ -43,4 +43,5 @@ fi
 echo 'CURRENT_WAL="'"$CHANGETO"'"' | tee "${HOME}/Downloads/shit/.cycle" &> /dev/null || exit 1
 
 # echo "${WALLS}/${CHANGETO}" # debugging purposes
-wal -i "${WALLS}/${CHANGETO}" --backend haishoku -b "#14161c" -e && "$HOME/.config/scripts/termite-wal.sh"
+# wal -i "${WALLS}/${CHANGETO}" --backend haishoku -b "#14161c" -e && "$HOME/.config/scripts/termite-wal.sh"
+feh --bg-scale "${WALLS}/${CHANGETO}"
